c868d01c86852eff4ef9e8963fcc5fde1b4ad363
TatyanaNikonova <99797822+TatyanaNikonova@users.noreply.github.com>
2022-06-06 21:55:51 +0300

    Made some change

--- start_of_game.py
+++ start_of_game.py
@@ -1,6 +1,7 @@
 def start_of_tic_tac_toe_game():
     
     mark = input('Вы будете играть крестиками (X) или ноликами (O)? ')
+    mark = mark.upper()
     
     while True:
         if mark == 'X':
@@ -8,5 +9,6 @@ def start_of_tic_tac_toe_game():
         if mark == 'O':
             return False
         mark = input('Вы ввели не те знаки. Выберите х или о: ')
+        mark = mark.upper()
 
 start_of_tic_tac_toe_game()
